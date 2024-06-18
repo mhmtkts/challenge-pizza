98924121deae51c12c56ae1de1f07e215eaf6c70
Mahmut Aktaş <mahmutaktas.m@gmail.com>
2024-06-19 02:53:20 +0300

    components  and pages added

---
+++ src/pages/OrderPizza.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import ProductInfo from "../components/Product-Info";
+
+function OrderPizza() {
+  return (
+    <>
+      <Header></Header>
+      <ProductInfo></ProductInfo>
+    </>
+  );
+}
+
+export default OrderPizza;
