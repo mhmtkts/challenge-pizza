0a0e60736632c47cc487a8cef1e99db8cbc6ac00
Mahmut Aktaş <mahmutaktas.m@gmail.com>
2024-06-21 18:58:10 +0300

    edited

--- cypress/e2e/pizza-test/pizza.cy.js
+++ cypress/e2e/pizza-test/pizza.cy.js
@@ -1,28 +1,33 @@
 beforeEach(() => {
-    cy.visit("http://localhost:5173/");
+  cy.visit("http://localhost:5173/");
+});
+
+describe("SANITY CHECK", () => {
+  it("open home page", () => {
+    cy.url().should("contain", "http://localhost:5173/");
   });
-  
-  describe("SANITY CHECK", () => {
-    it("open home page", () => {
-      cy.url().should("contain", "http://localhost:5173/");
-    });
+});
+
+describe("SUCCESS", () => {
+  it("open the order page", () => {
+    cy.get('[data-cy="route-orderpizza"]').as("orderpizza");
+    cy.get("@orderpizza").click();
+    cy.url().should("contain", "/orderpizza");
   });
-  
-  describe("SUCCESS", () => {
-    it("open the order page", () => {
-      cy.get('[data-cy="route-orderpizza"]').as("orderpizza");
-      cy.get("@orderpizza").click();
-      cy.url().should("contain", "/orderpizza");
-    });
-    it("submit form", () => {
-        cy.get('[data-cy="route-orderpizza"]').as("orderpizza");
-         cy.get("@orderpizza").click();
-    
-        cy.get('[data-cy= "input-size"]').check();
-        cy.get('select').select('İnce Hamur');
-        cy.get('input[type="checkbox"]').check(['Pepperoni', 'Sosis', 'Kanada Jambonu', 'Tavuk Izgara']);
-        cy.get('[data-cy = "input-name"]').type("Mahmut AKTAŞ");
-        cy.get('[data-cy = "input-orderbutton"]').click();
-        cy.url().should("contain", "/success");
-      });
-});   
+  it("submit form", () => {
+    cy.get('[data-cy="route-orderpizza"]').as("orderpizza");
+    cy.get("@orderpizza").click();
+
+    cy.get('[data-cy= "input-size"]').check();
+    cy.get("select").select("İnce Hamur");
+    cy.get('input[type="checkbox"]').check([
+      "Pepperoni",
+      "Sosis",
+      "Kanada Jambonu",
+      "Tavuk Izgara",
+    ]);
+    cy.get('[data-cy = "input-name"]').type("Mahmut AKTAŞ");
+    cy.get('[data-cy = "input-orderbutton"]').click();
+    cy.url().should("contain", "/success");
+  });
+});
